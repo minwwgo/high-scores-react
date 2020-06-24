a9a640aeeeec33b898cf30e3bae94fcbab55e5fc
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-06-24 17:14:37 +0100

    add table for wold score

--- src/WorldScore.js
+++ src/WorldScore.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const WorldScore = props => {
+const WorldScore = (props) => {
   const { scores } = props;
 
   return (
-    <div>
-      {scores.map(eachData => {
-        const result = eachData.scores.reduce((a, b) => {
-          return a.s > b.s ? a : b;
-        });
-        return (
-          <p>
-            {" "}
-            {result.n}
-            {result.s}
-          </p>
-        );
-      })}
+    <div className="card">
+      <h1 className="card-header bg-info  text-center">world score</h1>
+      <ul className="list-group list-group-flush">
+        {scores.map((eachData) => {
+          const result = eachData.scores.reduce((a, b) => {
+            return a.s > b.s ? a : b;
+          });
+          return (
+            <li className="list-group-item d-flex justify-content-around ">
+              <span>{result.n}</span>
+              <span>{result.s}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
